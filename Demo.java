82fe32974c2d0bee38b8a88832cf55c549336a6b
Silmiuww <nadiasalma078@gmail.com>
2025-11-25 19:19:22 +0700

    Polymorphic Arguments & instanceOf Langkah 3

--- Demo.java
+++ Demo.java
@@ -2,18 +2,15 @@ import java.util.ArrayList;
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom., M.Kom", "199402");
+        TenagaKependidikan tendik1 = new TenagaKependidikan("19940301", "Aida, A.Md", "Tenaga Administrasi");
 
-        Pegawai pegawai1 = dosen1;
+        train(dosen1);
+        train(tendik1);
+    }
 
-        System.out.println(pegawai1.nip);
-        System.out.println(pegawai1.nama);
-        pegawai1.displayInfo();
-
-        Dosen newDosen = (Dosen) pegawai1;
-
-        System.out.println(newDosen.nama);
-        System.out.println(newDosen.nidn);
-        newDosen.mengajar();
+    public static void train(Pegawai pegawai){
+        System.out.println("Memberikan pelatihan untuk pegawai");
+        pegawai.displayInfo();
     }
 }
 
